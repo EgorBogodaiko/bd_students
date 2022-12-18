0466f876ffa55c4c38b367d795daa336f1a0b749
Egor <1begosha@gmail.com>
2022-12-18 15:54:54 +0300

    controler&show bd ready

--- show_bd.py
+++ show_bd.py
@@ -1,2 +1,18 @@
-def show_all_bd():
+import csv
+
+
+def show_all_bd(db_name):
+    """принимает на вход адрес БД
+    выводит всю БД в консоль
+    ничего не возвращает
+    """
+    with open(db_name) as f:
+        reader = csv.reader(f,delimiter='|')
+        headers = next(reader)
+        print(headers)
+        for row in reader:
+            print(row)
     
+
+
+    return
